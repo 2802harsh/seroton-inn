8cd5d6a22e98ebdd4a9fc8d3e6b7839c8088713c
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2020-11-15 15:30:44 +0530

    create avatr component

--- src/components/compose-components/Compose.js
+++ src/components/compose-components/Compose.js
@@ -1,13 +1,15 @@
 import React from "react";
 import TextArea from "./TextArea";
+import Avatar from './Avatar';
 
 export default () => {
   return (
     <div class="compose">
       <div>
-        <div class="image-container">
+        {/* <div class="image-container">
           <img src="./name.png" alt="profilePicture"></img>
-        </div>
+        </div> */}
+        <Avatar />
         <TextArea />
       </div>
     </div>
